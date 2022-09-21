2b1d292f18f01439c756e5b714bed4e0283fa931
badre <bbennittoo@gmail.com>
2022-09-21 10:54:45 +0000

    V1

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -66,51 +66,7 @@ function CookieBanner() {
   if (cookies[COOKIES_ACCEPTED] === 'true' || isDeclined || typeof window === undefined) {
     return null;
   } else {
-    return (
-      <>
-        <div className={'fixed bottom-5 z-50 flex w-full justify-center'}>
-          <div
-            className={
-              'mx-4 flex max-w-3xl flex-col items-center justify-between gap-2 rounded-lg bg-gray-800 bg-opacity-60 p-2 backdrop-blur-lg sm:flex-row'
-            }
-          >
-            <div className={'flex items-center gap-2'}>
-              <img
-                src={'/images/hola-logo.svg'}
-                alt={'hola-logo'}
-                className={'h-6 w-6 animate-waving'}
-              />
-
-              <p className={'m-0 text-sm font-normal text-white'}>
-                We use cookies to personalize your browsing experience and analyze site traffic.
-                Read our{' '}
-                <a
-                  target={`_blank`}
-                  className={`font-bold`}
-                  href={`https://docs.google.com/document/d/12uQU7LbLUd0bY7Nz13-F9cua5Wk8mnRNBlyDzF6gRmo/edit`}
-                >
-                  privacy policy
-                </a>
-                &nbsp;for details on how we use this data.
-              </p>
-            </div>
-            <div className={`flex items-center gap-4`}>
-              <Button secondary={true} size={'small'} onClick={() => openSettings(true)}>
-                Decline
-              </Button>
-              <Button size={'small'} onClick={acceptAllCookies}>
-                Accept
-              </Button>
-            </div>
-          </div>
-        </div>
-        <CookieSettings
-          openSettings={isSettingsOpen}
-          setOpenSettings={openSettings}
-          decline={declineCookies}
-        />
-      </>
-    );
+    return <></>;
   }
 }
 
